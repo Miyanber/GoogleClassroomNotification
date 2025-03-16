c1de88c8fd66ce2ce89eed54f334e3fa20caa83d
Miyanber <miyanba2984h@gmail.com>
2025-03-16 22:38:33 +0900

    GoogleClassroomのデータ取得をOAuth2に

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function doGet(e) {
     let template = HtmlService.createTemplateFromFile("index");
     const accountEmail = Session.getActiveUser().getEmail();
 
-    var service = getService(accountEmail);
+    var service = getOAuthService(accountEmail);
     if (service.hasAccess()) {
         const spreadsheet = SpreadsheetApp.openById('1ii2MBtSLK5vmq9PNP6y0RBtvQsxwefstCfQ_HNoIFyI');
         const sheet = spreadsheet.getSheetByName('Sheet1');
@@ -22,10 +22,10 @@ function doGet(e) {
         if (exists === true) {
             const email = values[0][1];
             const timeDate = new Date(values[0][2]);
-            const time = `${timeDate.getHours()}:${timeDate.getMinutes()}`;
+            const time = `${zeroPadding(timeDate.getHours())}:${zeroPadding(timeDate.getMinutes())}`;
             template.email = email;
             template.time = time;
-            Logger.log(`Email: ${template.email}, Time: ${ template.time}`);
+            Logger.log(`Email: ${template.email}, Time: ${template.time}`);
         } else {
             template.email = "";
             template.time = "";
@@ -62,7 +62,8 @@ function saveUser(time, email) {
 
     Logger.log(`Saved: ${email}, ${time}`);
 
-    const service = getService(accountEmail);
+    const service = getOAuthService(accountEmail);
+
     if (service.hasAccess()) {
         Logger.log('already authorized');
         return null;
@@ -79,54 +80,64 @@ function getAppUrl() {
 function main() {
     const spreadsheet = SpreadsheetApp.openById('1ii2MBtSLK5vmq9PNP6y0RBtvQsxwefstCfQ_HNoIFyI');
     const sheet = spreadsheet.getSheetByName('Sheet1');
-    const data = sheet.getRange(2, 1, sheet.getLastRow(), sheet.getLastColumn()).getValues();
+    const data = sheet.getRange(2, 1, sheet.getLastRow() - 1, sheet.getLastColumn()).getValues();
 
     data.forEach(row => {
+        const accountEmail = row[0];
         const email = row[1];
-        const time = row[2];
-        const hours = parseInt(time.split(":")[0], 10);
-        const minutes = parseInt(time.split(":")[1], 10);
+        const time = new Date(row[2]);
 
-        if (now.getHours() == hours && (now.getMinutes() == minutes || now.getMinutes() == minutes)) {
-            sendReminder(email);
+        if (now.getHours() == time.getHours() && now.getMinutes() == time.getMinutes()) {
+            sendReminder(accountEmail, email);
         }
 
-        Logger.log(`Email: ${email}, Time: ${time}, TriggerTime: ${triggerTime}`);
+        Logger.log(`Email: ${email}, Time: ${time}`);
     });
 }
 
-function sendReminder(email) {
+function sendReminder(accountEmail, email) {
     const recipient = email; // 送信先メールアドレス
 
     const subject = `【リマインダー】本日更新された GoogleClassroom のお知らせ`;
 
     const template = HtmlService.createTemplateFromFile('mail_template');
 
-    template.courses = getTodayUpdates();
+    const service = getOAuthService(accountEmail);
+    if (service.hasAccess()) {
+        const accessToken = service.getAccessToken();
+        template.courses = getTodayUpdates(accessToken);
+        template.accessToken = accessToken;
+    } else {
+        template.courses = [];
+        template.accessToken = "";
+    }
 
     const html = template.evaluate().getContent();
 
-    const bookIcon = DriveApp.getFileById("11lg5pY4xeIto0FOgm4wxkJFIlQgpdoaR").getBlob();
-    const assignmentIcon = DriveApp.getFileById("1i25KhzDgdjsGiSBpQ6SuGf5Pnn0bxyrY").getBlob();
-
     // メール送信
-    MailApp.sendEmail(recipient, subject, html, { htmlBody: html, inlineImages: { bookIcon: bookIcon, assignmentIcon: assignmentIcon } });
+    MailApp.sendEmail(recipient, subject, html, { htmlBody: html });
 }
 
-function getTodayUpdates() {
-    now.setDate(now.getDate() - 1);
-    now.setHours(now.getHours() + 9);
-    const todayStr = now.toISOString().split("T")[0]; // YYYY-MM-DD
-    now.setHours(now.getHours() - 9);
-    Logger.log(todayStr);
+function getTodayUpdates(accessToken) {
+    now.setDate(now.getDate());
 
     let courses = [];
-    for (const id of Classroom.Courses.list({ 'courseStates': ["ACTIVE"] }).courses.map(course => course.id)) {
-        Logger.log(Classroom.Courses.get(id));
+
+    const fetchedCourses = fetchWrapper(accessToken, "courses");
+
+    for (const id of fetchedCourses.courses.map(course => course.id)) {
+        if (getCache(id) != null) {
+            courses.push(JSON.parse(getCache(id)));
+            continue;
+        }
+
+        const classroom = fetchWrapper(accessToken, `courses/${id}`);
+        Logger.log(`Classroom: ${classroom.name}`);
+
         let coursePosts = [];
-        const courseAnnounces = Classroom.Courses.Announcements.list(id).announcements;
-        const courseWorks = Classroom.Courses.CourseWork.list(id).courseWork;
-        const courseWorkMaterials = Classroom.Courses.CourseWorkMaterials.list(id).courseWorkMaterial;
+        const courseAnnounces = fetchWrapper(accessToken, `courses/${id}/announcements`).announcements;
+        const courseWorks = fetchWrapper(accessToken, `courses/${id}/courseWork`).courseWork;
+        const courseWorkMaterials = fetchWrapper(accessToken, `courses/${id}/courseWorkMaterials`).courseWorkMaterial;
         if (courseAnnounces != null) {
             coursePosts.push(...courseAnnounces);
         }
@@ -137,7 +148,7 @@ function getTodayUpdates() {
             coursePosts.push(...courseWorkMaterials);
         }
         coursePosts = coursePosts.filter(
-            post => post.updateTime.split("T")[0] == todayStr
+            post => isLatest(now, new Date(post.updateTime))
         ).map(post => {
             if (post.workType != null) {
                 post["type"] = "WORKS";
@@ -150,12 +161,66 @@ function getTodayUpdates() {
         });
         const obj = {
             "courseId": id,
-            "courseName": Classroom.Courses.get(id).name,
+            "courseName": classroom.name,
             "posts": coursePosts
         }
         courses.push(obj);
+        setCache(id, JSON.stringify(obj), 300);
     }
     return courses;
+}
+
+/**
+ * 
+ * @param {string} accessToken 
+ * @param {string} url 
+ * @param {object} payload 
+ */
+function fetchWrapper(accessToken, url) {
+    const options = {
+        method: "GET",
+        headers: {
+            'Authorization': 'Bearer ' + accessToken,
+        }
+    }
+
+    try {
+        const response = UrlFetchApp.fetch("https://classroom.googleapis.com/v1/" + url, options);
+        return JSON.parse(response.getContentText());
+    } catch (e) {
+        Logger.log(e);
+        return null;
+    }
+}
+
+function testMail() {
+    const spreadsheet = SpreadsheetApp.openById('1ii2MBtSLK5vmq9PNP6y0RBtvQsxwefstCfQ_HNoIFyI');
+    const sheet = spreadsheet.getSheetByName('Sheet1');
+    let range;
+    const accountEmail = Session.getActiveUser().getEmail();
+    let values = [];
+
+    const hasAlreadySaved = sheet.getRange(2, 1, sheet.getLastRow() - 1, sheet.getLastColumn()).getValues().some((row, index) => {
+        if (row[0] == accountEmail) {
+            range = sheet.getRange(index + 2, 1, 1, 3);
+            values = range.getValues()[0];
+            return true;
+        }
+    })
+
+    if (!hasAlreadySaved) {
+        return false;
+    }
+
+    const email = values[1];
+    const html = 'メール送信テストです。'
+    try {
+        MailApp.sendEmail(email, "【Classroom Summary Notification】テストメール送信", html);
+        return true;
+    } catch (e) {
+        Logger.log(e);
+        return false;
+    }
 }
 
 function logScopes() {
@@ -166,9 +231,30 @@ function toBrTag(text) {
     return text.replace(/\n/g, "<br>");
 }
 
-// ここから下はOAuth2関連の関数
+function isLatest(today, target) {
+    const diff = Math.abs(today.getTime() - target.getTime());
+    return diff < 1000 * 60 * 60 * 24; // 1日;
+}
 
-function getService(accountEmail) {
+function setCache(key, value) {
+    CacheService.getScriptCache().put(key, value);
+}
+
+function getCache(key) {
+    CacheService.getScriptCache().get(key);
+}
+
+function zeroPadding(numbner) {
+    if (numbner < 10) {
+        return "0" + numbner;
+    }
+    return numbner;
+}
+
+// ##### ここから下はOAuth2関連の関数 #####
+
+
+function getOAuthService(accountEmail) {
     if (accountEmail == null) {
         return null;
     }
@@ -179,7 +265,7 @@ function getService(accountEmail) {
         .setClientSecret(PropertiesService.getScriptProperties().getProperty('CLIENT_SECRET'))
         .setCallbackFunction('authCallback')
         .setPropertyStore(PropertiesService.getUserProperties())
-        .setScope('https://www.googleapis.com/auth/classroom.courses.readonly https://www.googleapis.com/auth/classroom.announcements.readonly https://www.googleapis.com/auth/classroom.rosters.readonly https://www.googleapis.com/auth/classroom.coursework.students.readonly https://www.googleapis.com/auth/classroom.courseworkmaterials.readonly')
+        .setScope('https://www.googleapis.com/auth/classroom.courses.readonly https://www.googleapis.com/auth/classroom.announcements https://www.googleapis.com/auth/classroom.profile.photos https://www.googleapis.com/auth/classroom.coursework.me https://www.googleapis.com/auth/classroom.courseworkmaterials.readonly ')
 
         // Requests offline access.
         .setParam('access_type', 'offline')
@@ -193,27 +279,18 @@ function getService(accountEmail) {
 
 // OAuth2認証のコールバック関数
 function authCallback(request) {
-    var service = getService(Session.getActiveUser().getEmail());
+    var service = getOAuthService(Session.getActiveUser().getEmail());
     var isAuthorized = service.handleCallback(request);
     if (isAuthorized) {
-        const spreadsheet = SpreadsheetApp.openById('1ii2MBtSLK5vmq9PNP6y0RBtvQsxwefstCfQ_HNoIFyI');
-        const sheet = spreadsheet.getSheetByName('Sheet1');
-        sheet.getRange(2, 1, sheet.getLastRow(), sheet.getLastColumn()).getValues().some((row, index) => {
-            if (row[0] == accountEmail) {
-                range = sheet.getRange(index + 2, 4);
-                range.setValue('True');
-                return true;
-            }
-        })
         return HtmlService.createHtmlOutput('認証が完了しました。このウィンドウを閉じてください。');
     } else {
-        return HtmlService.createHtmlOutput('認証に失敗しました。');
+        return HtmlService.createHtmlOutput('認証に失敗しました。元の画面にもどり、再設定を行ってください。');
     }
 }
 
 // 認証を開始するための関数
 function startOAuth2(email) {
-    var service = getService(email);
+    var service = getOAuthService(email);
     var authorizationUrl = service.getAuthorizationUrl(email);
     Logger.log('認証URL: ' + authorizationUrl);
     return authorizationUrl;
